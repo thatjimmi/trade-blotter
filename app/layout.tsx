354bba22b91314a2eba37fd8d913b0bc4211808f
Jimmi Mortensen <52856973+thatjimmi@users.noreply.github.com>
2025-02-16 01:33:27 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,42 +1,42 @@
 import "./globals.css";
-import localFont from 'next/font/local';
+import localFont from "next/font/local";
 
 const cabinetGrotesk = localFont({
   src: [
     {
-      path: '../public/fonts/CabinetGrotesk-Thin.woff',
-      weight: '100'
+      path: "../public/fonts/CabinetGrotesk-Thin.woff",
+      weight: "100",
     },
     {
-      path: '../public/fonts/CabinetGrotesk-ExtraLight.woff',
-      weight: '200'
+      path: "../public/fonts/CabinetGrotesk-Extralight.woff",
+      weight: "200",
     },
     {
-      path: '../public/fonts/CabinetGrotesk-Light.woff',
-      weight: '300'
+      path: "../public/fonts/CabinetGrotesk-Light.woff",
+      weight: "300",
     },
     {
-      path: '../public/fonts/CabinetGrotesk-Regular.woff',
-      weight: '400'
+      path: "../public/fonts/CabinetGrotesk-Regular.woff",
+      weight: "400",
     },
     {
-      path: '../public/fonts/CabinetGrotesk-Medium.woff',
-      weight: '500'
+      path: "../public/fonts/CabinetGrotesk-Medium.woff",
+      weight: "500",
     },
     {
-      path: '../public/fonts/CabinetGrotesk-Bold.woff',
-      weight: '700'
+      path: "../public/fonts/CabinetGrotesk-Bold.woff",
+      weight: "700",
     },
     {
-      path: '../public/fonts/CabinetGrotesk-ExtraBold.woff',
-      weight: '800'
+      path: "../public/fonts/CabinetGrotesk-ExtraBold.woff",
+      weight: "800",
     },
     {
-      path: '../public/fonts/CabinetGrotesk-Black.woff',
-      weight: '900'
-    }
+      path: "../public/fonts/CabinetGrotesk-Black.woff",
+      weight: "900",
+    },
   ],
-  variable: '--font-cabinet'
+  variable: "--font-cabinet",
 });
 
 export default function RootLayout({
@@ -46,11 +46,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={cabinetGrotesk.variable}
-      >
-        {children}
-      </body>
+      <body className={cabinetGrotesk.variable}>{children}</body>
     </html>
   );
 }
